b4c9efbfd3f1b6f00329a262383c997f84d0b215
Zeryuix <leo.mermet91@gmail.com>
2026-01-28 11:30:07 -0500

    feature(mail-confirmation): add email verification on registration

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,13 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable implements JWTSubject
+class User extends Authenticatable implements JWTSubject, MustVerifyEmail
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -24,6 +24,7 @@ class User extends Authenticatable implements JWTSubject
         'password',
         'profile_picture',
         'role',
+        'email_verified_at',
     ];
 
     /**
