de21538f2bf09b26b13170e798b3716ef1e3a125
Nathan FOURRIER <nfourrier@citypassenger.com>
2026-04-24 10:56:06 +0200

    locdetail: extract ldNetworks IIFE to location-details-networks.js (T4.8 step 2/6)

--- resources/views/location-details.blade.php
+++ resources/views/location-details.blade.php
@@ -488,5 +488,6 @@
     window.APP_I18N.common = @json(__('common'));
 </script>
 <script src="/assets/js/mw-primitives.js?v={{ filemtime(public_path('assets/js/mw-primitives.js')) }}"></script>
+<script src="/assets/js/location-details-networks.js?v={{ filemtime(public_path('assets/js/location-details-networks.js')) }}"></script>
 <script src="/assets/js/location-details.js?v={{ filemtime(public_path('assets/js/location-details.js')) }}"></script>
 @endpush
